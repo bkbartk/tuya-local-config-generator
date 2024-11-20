9c1fc6527cc82d0a9644def3f5c7ad924bc18000
Bart Kock <bart.kock@sienn.com>
2024-11-20 22:10:09 +0100

    Add project files.

---
+++ tuyalocalconfiggenerator/Classes/Output.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace tuyalocalconfiggenerator.Classes
+{
+    internal class Output
+    {
+        public class Yaml
+        {
+            public Yaml()
+            {
+                Products = new HashSet<Product>();
+                SecondaryEntities = new HashSet<SecondaryEntity>();
+            }
+            [JsonPropertyName("name")]
+            public string Name { get; set; }
+
+            [JsonPropertyName("products")]
+            public ICollection<Product> Products { get; set; }
+
+            [JsonPropertyName("primary_entity")]
+            public PrimaryEntity PrimaryEntity { get; set; }
+
+            [JsonPropertyName("secondary_entities")]
+            public ICollection<SecondaryEntity> SecondaryEntities { get; set; }
+        }
+
+        public partial class PrimaryEntity
+        {
+            [JsonPropertyName("entity")]
+            public string Entity { get; set; }
+
+            [JsonPropertyName("dps")]
+            public PrimaryEntityDp[] Dps { get; set; }
+        }
+
+        public partial class PrimaryEntityDp
+        {
+            [JsonPropertyName("id")]
+            public long Id { get; set; }
+
+            [JsonPropertyName("type")]
+            public string Type { get; set; }
+
+            [JsonPropertyName("name")]
+            public string Name { get; set; }
+
+            [JsonPropertyName("optional")]
+            public bool? Optional { get; set; }
+
+            [JsonPropertyName("mapping")]
+            public Mapping[] Mapping { get; set; }
+
+            [JsonPropertyName("hidden")]
+            public bool? Hidden { get; set; }
+        }
+
+        public partial class Mapping
+        {
+            [JsonPropertyName("dps_val")]
+            public string DpsVal { get; set; }
+
+            [JsonPropertyName("value")]
+            public string Value { get; set; }
+        }
+
+        public partial class Product
+        {
+            [JsonPropertyName("id")]
+            public string Id { get; set; }
+
+            [JsonPropertyName("name")]
+            public string Name { get; set; }
+        }
+
+        public partial class SecondaryEntity
+        {
+            [JsonPropertyName("entity")]
+            public string Entity { get; set; }
+
+            [JsonPropertyName("class")]
+            public string Class { get; set; }
+
+            [JsonPropertyName("dps")]
+            public SecondaryEntityDp[] Dps { get; set; }
+
+            [JsonPropertyName("name")]
+            public string Name { get; set; }
+
+            [JsonPropertyName("icon")]
+            public string Icon { get; set; }
+
+            [JsonPropertyName("category")]
+            public string Category { get; set; }
+        }
+
+        public partial class SecondaryEntityDp
+        {
+            [JsonPropertyName("id")]
+            public long Id { get; set; }
+
+            [JsonPropertyName("type")]
+            public string Type { get; set; }
+
+            [JsonPropertyName("name")]
+            public string Name { get; set; }
+
+            [JsonPropertyName("unit")]
+            public string Unit { get; set; }
+
+            [JsonPropertyName("class")]
+            public string Class { get; set; }
+
+            [JsonPropertyName("optional")]
+            public bool? Optional { get; set; }
+
+            [JsonPropertyName("mapping")]
+            public Mapping[] Mapping { get; set; }
+        }
+
+    }
+}
---
+++ tuyalocalconfiggenerator/MainWindow.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using YamlDotNet.Serialization;
+using static tuyalocalconfiggenerator.Classes.Output;
+
+namespace tuyalocalconfiggenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainSettings _MainSettings = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = _MainSettings;
+        }
+
+        private void txtVacuum_Click(object sender, RoutedEventArgs e)
+        {
+            var operation = new ParameterizedThreadStart(obj => txtVacuum_Thread());
+            Thread thread = new Thread(operation);
+            thread.Start();
+        }
+        private async void txtVacuum_Thread()
+        {
+            try
+            {
+                var jsonMain = System.Text.Json.JsonSerializer.Deserialize<JsonMain>(_MainSettings.Input);
+                var jsonSub = System.Text.Json.JsonSerializer.Deserialize<JsonSub>(jsonMain.result.model);
+                var output = new Classes.Output.Yaml();
+                output.Name = "Robot vacuum";
+                var product = new Classes.Output.Product();
+                product.Id = "Prod Id";
+                product.Name = "Robot vacuum";
+                output.Products.Add(product);
+
+                var serializer = new YamlDotNet.Serialization.Serializer();
+                var yaml = new StringBuilder();
+
+                await using var textWriter = new StringWriter(yaml);
+
+                serializer.Serialize(textWriter, output, output.GetType());
+                _MainSettings.Output = yaml.ToString();
+            }
+            catch (Exception ex) {
+                //TODO errorhandling
+                _MainSettings.Output = ex.ToString();
+            }     
+        }
+
+        private void Hotfix_Click(object sender, RoutedEventArgs e)
+        {
+            txtOutput.Text = _MainSettings.Output;
+        }
+    }
+
+    public class MainSettings
+    {
+        public string Input { get; set; }
+        public string Output { get; set; }
+    }
+
+    //main result
+    public class JsonMainResult
+    {
+        public string model { get; set; }
+    }
+
+    public class JsonMain
+    {
+        public JsonMainResult result { get; set; }
+        public bool success { get; set; }
+        public long t { get; set; }
+        public string tid { get; set; }
+    }
+    //main result
+
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Extensions
+    {
+        public string iconName { get; set; }
+        public string attribute { get; set; }
+        public string scope { get; set; }
+    }
+
+    public class Property
+    {
+        public int abilityId { get; set; }
+        public string accessMode { get; set; }
+        public string code { get; set; }
+        public string description { get; set; }
+        public Extensions extensions { get; set; }
+        public string name { get; set; }
+        public TypeSpec typeSpec { get; set; }
+    }
+
+    public class JsonSub
+    {
+        public string modelId { get; set; }
+        public List<Service> services { get; set; }
+    }
+
+    public class Service
+    {
+        public List<object> actions { get; set; }
+        public string code { get; set; }
+        public string description { get; set; }
+        public List<object> events { get; set; }
+        public string name { get; set; }
+        public List<Property> properties { get; set; }
+    }
+
+    public class TypeSpec
+    {
+        public string type { get; set; }
+        public List<string> range { get; set; }
+        public int? max { get; set; }
+        public int? min { get; set; }
+        public int? scale { get; set; }
+        public int? step { get; set; }
+        public string unit { get; set; }
+        public int? maxlen { get; set; }
+        public List<string> label { get; set; }
+    }
+
+
+
+
+}
